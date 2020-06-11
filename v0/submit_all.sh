5d7b4b658e94beb105927591ce8daed35494d900
shkawada <shin-ichi.kawada@desy.de>
2020-06-11 19:19:02 +0200

    added files for mass production

---
+++ v0/submit_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+condor_submit exec1.submit
+condor_submit exec2.submit
+condor_submit exec3.submit
+condor_submit exec4.submit
+condor_submit exec5.submit
+condor_submit exec6.submit
+condor_submit exec7.submit
+condor_submit exec8.submit
+condor_submit exec9.submit
+condor_submit exec10.submit
+condor_submit exec11.submit
+condor_submit exec12.submit
+condor_submit exec13.submit
+condor_submit exec14.submit
+
